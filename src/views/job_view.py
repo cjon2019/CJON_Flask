b47bffbe0f34ad82ff360a8ca6afad3276b3916f
Rion5 <31965265+Rion5@users.noreply.github.com>
2019-04-23 09:41:46 -0400

    Adding /api/v1/jobs/test route

--- src/views/job_view.py
+++ src/views/job_view.py
@@ -124,11 +124,69 @@ def job_state_and_position_query(state = None, position = None):
 
 @jobs_api.route('/test',methods=['GET'])
 def test():
-    final_url = 'https://data.usajobs.gov/api/Search?ResultsPerPage=1000&LocationName=Colorado'
+    final_url = 'https://data.usajobs.gov/api/Search?ResultsPerPage=1000&LocationName=Indiana&PositionTitle=IT'
     response = requests.get(final_url, headers=headers)
     # Convert to Python Dictionary
     data = response.json()
+
+    # Number of Jobs found with query
+    result_count = data['SearchResult']['SearchResultCount']
+    print('Result Count:' + str(result_count))
+    # Creating a List, or Dictionaries
+    my_list = [{
+        'matched_object_id': 0,
+        'position_title' : 'Test Position',
+        'job_summary': 'Test Summary',
+        'position_location' : 'Test Location',
+        'position_start_date' : '01/01/2019',
+        'position_end_date' : '12/31/2019',
+        'min_range': 0,
+        'max_range': 100,
+        'rate_interval_code': 'Per Year'
+    }]
+    state = 'Indiana'
+    for i in range(result_count):
+        if state in data['SearchResult']['SearchResultItems'][i]['MatchedObjectDescriptor']['PositionLocation'][0]['LocationName']:
+            matched_object_id = data['SearchResult']['SearchResultItems'][i]['MatchedObjectId']
+            position_title = data['SearchResult']['SearchResultItems'][i]['MatchedObjectDescriptor']['PositionTitle']
+            job_summary = data['SearchResult']['SearchResultItems'][i]['MatchedObjectDescriptor']['UserArea']['Details']['JobSummary']
+            position_location = data['SearchResult']['SearchResultItems'][i]['MatchedObjectDescriptor']['PositionLocation'][0]['LocationName']
+            position_start_date = data['SearchResult']['SearchResultItems'][i]['MatchedObjectDescriptor']['PositionStartDate']
+            position_end_date = data['SearchResult']['SearchResultItems'][i]['MatchedObjectDescriptor']['PositionEndDate']
+            min_range = 0
+            try:
+                min_range = data['SearchResult']['SearchResultItems'][i]['MatchedObjectDescriptor']['PositionRemuneration'][0]['MinimumRange']
+            except:
+                min_range = 0
+                print('No Min Salary data')
+            
+            max_range = 0
+            try:
+               max_range = data['SearchResult']['SearchResultItems'][i]['MatchedObjectDescriptor']['PositionRemuneration'][0]['MaximumRange']
+            except:
+                max_range = 0
+                print('No Max Salary data')
+            rate_interval_code = 'NA'
+            try:
+                rate_interval_code = data['SearchResult']['SearchResultItems'][i]['MatchedObjectDescriptor']['PositionRemuneration'][0]['RateIntervalCode']
+            except:
+                print('No Rate of Interval data')
+            my_list.append({
+                'matched_object_id': matched_object_id,
+                'position_title': position_title,
+                'job_summary': job_summary,
+                'position_location' : position_location,
+                'position_start_date' : position_start_date,
+                'position_end_date' : position_end_date,
+                'min_range': min_range,
+                'max_range': max_range,
+                'rate_interval_code': rate_interval_code
+            })
+
+
+    print(my_list[1])
     # Converts JSON back into a string, so that the browser can render it
-    json_formatted_string = json.dumps(data)
-    print(type(json_formatted_string))
+    # json_formatted_string = json.dumps(data)  # data contains all JSON data of the job posting
+    json_formatted_string = json.dumps(my_list) # my_list contains only the JSON data that we care about
+    print(type(json_formatted_string))          # type == str
     return json_formatted_string
